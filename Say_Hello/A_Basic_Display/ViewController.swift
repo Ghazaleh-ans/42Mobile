14b32a75fedba67a98d8f6340eb34baf739fc104
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-04-13 12:40:28 +0200

    say hello is done

--- Say_Hello/A_Basic_Display/ViewController.swift
+++ Say_Hello/A_Basic_Display/ViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isOn = false
+    @IBOutlet weak var textLabel: UILabel!
     @IBAction func clickMeButton(_ sender: Any) {
+        isOn.toggle()
+        textLabel.text = isOn ? "Hello World" : "A simple text"
         print("Button pressed")
     }
     override func viewDidLoad() {
