22ec4bf2d723ac6f6b18d5c11ecc92f2026bfe98
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-04-13 12:31:59 +0200

    first commit

---
+++ Say_Hello/A_Basic_Display/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  A_Basic_Display
+//
+//  Created by Ghazall on 13.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func clickMeButton(_ sender: Any) {
+        print("Button pressed")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
