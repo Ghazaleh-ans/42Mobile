6596fa4ed7301839786d2150aef2c0fc66ec5c41
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-04-13 15:39:21 +0200

    input is updated

--- calculator_app/calculator_app/ViewController.swift
+++ calculator_app/calculator_app/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var first = true
     @IBOutlet weak var inputVal: UILabel!
     @IBOutlet weak var resVal: UILabel!
 
     @IBAction func inputButtonPressed(_ sender: UIButton) {
+        let newText = sender.titleLabel?.text ?? ""
+        if first {
+            inputVal.text = newText
+            first = false
+        }
+        else {
+            inputVal.text = (inputVal.text ?? "") + newText
+        }
         print("button pressed :\(sender.titleLabel!.text!)")
     }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        print("button pressed :\(sender.titleLabel!.text!)")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
