9086c1db59ce01d1e59695a7d3a59435aaa53127
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-04-13 14:43:21 +0200

    ex02 is done

--- Calculator/Calculator_Layout/ViewController.swift
+++ Calculator/Calculator_Layout/ViewController.swift
@@ -11,22 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func pressedNumber(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        print("button pressed :\(sender.titleLabel!.text!)")
     }
     
     @IBAction func decimalPressed(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        print("button pressed :\(sender.titleLabel!.text!)")
     }
     @IBAction func pressedOperator(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        print("button pressed :\(sender.titleLabel!.text!)")
     }
     
     @IBAction func ACButton(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        print("button pressed :\(sender.titleLabel!.text!)")
     }
     
     @IBAction func CButton(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        print("button pressed :\(sender.titleLabel!.text!)")
     }
     
     override func viewDidLoad() {
